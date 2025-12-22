9216cac21fedb191fbd2afcb35e756a2820deec1
akg807 <akshatgarg110@gmail.com>
2025-12-22 20:42:51 +0530

    added vendor requests approval

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -14,7 +14,7 @@ class RequestsController < ApplicationController
         @apartment = Apartment.new(apartment_params)
         @apartment.save
         @request.instance_id = @apartment.id
-        @request.eta = 3.months.from_now
+        @request.eta = 3.months.from_nowbr
         array_of_new_request << @apartment
       when "user"
         @request.eta = 1.month.from_now
@@ -22,6 +22,14 @@ class RequestsController < ApplicationController
         @request.eta = 7.days.from_now
       when "other"
         @request.eta = 15.days.from_now
+      when "vendor"
+        @vendor = Vendor.new(vendor_params)
+        rfid =  SecureRandom.uuid
+        @vendor.v_rfid = rfid
+        @vendor.save
+        @request.instance_id = @vendor.id
+        @request.eta = 15.days.from_now
+        array_of_new_request << @vendor
       end
 
 
@@ -60,13 +68,13 @@ class RequestsController < ApplicationController
   end
 
   def request_approval
-    @user = User.find_by(email: params[:email])
+    @admin = User.find_by(email: params[:email])
     @reference_number = params[:reference_number]
     @is_approved = params[:is_approved]
 
-    return render json: { message: "Invalid Email" } if @user.nil?
+    return render json: { message: "Invalid Email" } if @admin.nil?
 
-    return render json: { message: "Access Denied" } if @user.role != "Admin"
+    return render json: { message: "Access Denied" } if @admin.role != "Admin"
 
     @request = Request.find_by(reference_number: @reference_number)
     return render json: { message: "Invalid reference number" } if @request.nil?
@@ -77,6 +85,17 @@ class RequestsController < ApplicationController
 
     elsif @is_approved == true
       @request.update(status: "APPROVED")
+      if @request.request_type == "apartment"
+        @apartment = Apartment.find_by(id: @request.instance_id)
+        @apartment.update(approved: true)
+        @user = User.find_by(apartment_id: @apartment.id, role: "Owner")
+        return render json: { message: "Request APPROVED", user_email_sent: "#{@user.email}" }
+
+      elsif @request.request_type == "vendor"
+        @vendor = Vendor.find_by(id: @request.instance_id)
+        @vendor.update(approved: true)
+        return render json: { message: "Vendor Request approved" }
+      end
       render json: { message: "Request APPROVED" }
 
     else render json: { message: "Invalid parameters" }
@@ -95,5 +114,9 @@ class RequestsController < ApplicationController
 
   def user_params
     params.require(:users).permit(:email)
+  end
+
+  def vendor_params
+    params.require(:vendor).permit(:name, :phone_number, :amenity_id)
   end
 end
